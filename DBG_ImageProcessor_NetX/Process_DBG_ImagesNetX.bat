107b2f754d9a66363e93fd5f69f6366c8bd9dd12
Rick Levy <ricklevy21@gmail.com>
2026-03-16 11:07:20 -0600

    makes list and then calls api

--- DBG_ImageProcessor_NetX/Process_DBG_ImagesNetX.bat
+++ DBG_ImageProcessor_NetX/Process_DBG_ImagesNetX.bat
@@ -1,22 +1,25 @@
 @echo
+REM Move all renamed JPG files to their temporary processing location
+for /R "Q:\Research\Images(new)\MuseumSpecimens\DBG\DBG_Specimens\DBG_Incoming" %%F in (DBG-F-??????.jpg) do (
+    move "%%F" "Q:\Research\Images(new)\MuseumSpecimens\DBG\DBG_Specimens\DBG_JPEG_temp"
+)
+REM Move all renamed CR2 files to their temporary processing location
+for /R "Q:\Research\Images(new)\MuseumSpecimens\DBG\DBG_Specimens\DBG_Incoming" %%F in (DBG-F-??????.CR2) do (
+    move "%%F" "Q:\Research\Images(new)\MuseumSpecimens\DBG\DBG_Specimens\DBG_ToBeConverted"
+)
+
+REM Create DNG derivatives from CR2s
 for %%a in ("Q:\Research\Images(new)\MuseumSpecimens\DBG\DBG_Specimens\DBG_ToBeConverted\*.cr2") do (
     "C:\Program Files\Adobe\Adobe DNG Converter\Adobe DNG Converter.exe" -c -fl -cr2.4 -u -d "Q:\Research\Images(new)\MuseumSpecimens\DBG\DBG_Specimens\DBG_ToBeConverted" "%%a"
 )
 
-REM Path to Adobe Photoshop executable
-set photoshop_path="C:\Program Files\Adobe\Adobe Photoshop 2025\Photoshop.exe"
 
-REM Path to the JavaScript script file
-set script_path="C:\DBG_ImageProcessor\photoshop.jsx"
+REM Copy all JPGs to Local NetX Folder
+xcopy "Q:\Research\Images(new)\MuseumSpecimens\DBG\DBG_Specimens\DBG_JPEG_temp\*" "Q:\Research\Images(new)\MuseumSpecimens\DBG\DBG_Specimens\DBG_NetXIO"
 
-REM Execute the script in Photoshop
-%photoshop_path% %script_path%
 
-REM Forcefully close Photoshop after script execution
-taskkill /IM Photoshop.exe /F
-
-REM Move all web versions to local DBG NetX Import Folder
-move /y Q:\Research\Images(new)\MuseumSpecimens\DBG\To_Be_Compressed\JPEG\*.jpg Q:\Research\Images(new)\MuseumSpecimens\DBG\DBG_NetxIO
+REM Save list of all JPG file names copied to NetXIO
+dir /b "Q:\Research\Images(new)\MuseumSpecimens\DBG\DBG_Specimens\DBG_JPEG_temp\*.JPG" > "C:\DBG_ImageProcessor\fileList.txt"
 
 REM Run NetX IO Import
 java -Xms512M -Xmx2048M -cp "C:\Users\richard.levy\NetxIO\netxio-4.3.2.jar" com.netxposure.external.client.io.NetxIO -config "C:\Users\richard.levy\NetxIO\netxio_dbg_Import.properties"
@@ -28,8 +31,11 @@ REM Move the .CR2 raw files into the RAW All folder and overwrite already existi
 move /y Q:\Research\Images(new)\MuseumSpecimens\DBG\DBG_Specimens\DBG_ToBeConverted\*.CR2 Q:\Research\Images(new)\MuseumSpecimens\DBG\DBG_Specimens\DBG_RAW
 
 REM Move the .JPG raw files into the JPG All folder and overwrite already existing
-move /y Q:\Research\Images(new)\MuseumSpecimens\DBG\DBG_Specimens\DBG_ToBeCompressed\*.JPG Q:\Research\Images(new)\MuseumSpecimens\DBG\DBG_Specimens\DBG_JPEG
+move /y Q:\Research\Images(new)\MuseumSpecimens\DBG\DBG_Specimens\DBG_JPEG_temp\*.JPG Q:\Research\Images(new)\MuseumSpecimens\DBG\DBG_Specimens\DBG_JPEG
 
-REM Process Complete. Images on server ready for mapping on Mycoportal
+REM Create the Media Mapping File
+python "C:\DBG_ImageProcessor\scripts\createMediaFileMapping_NetXAPI_list.py"
+
+REM Process Complete. Images on server ready for mapping on Mycoportal using the Extended Data Import Tool.
 
 pause
